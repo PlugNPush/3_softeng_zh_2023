e0080c2cac9327af2adbce8c3df632a6bbf89563
Remo Laurin Senekowitsch <senekrem@students.zhaw.ch>
2023-10-25 11:00:05 +0200

    Rate limit POST requests (!35)

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,10 @@
-use axum::Router;
+use axum::{error_handling::HandleErrorLayer, http, Router};
 use clap::Parser;
 use server::{config::Config, docs::docs_handler, frontend::frontend_handler, router::api_router};
 use std::net::SocketAddr;
 use tokio::signal;
+use tower::{BoxError, ServiceBuilder};
+use tower_governor::{errors::display_error, governor::GovernorConfigBuilder, GovernorLayer};
 use tower_http::{compression::CompressionLayer, trace::TraceLayer};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
@@ -15,17 +17,36 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
+    // rate limiter stuff is mostly copied from the example
+    // https://docs.rs/tower_governor/0.1.0/tower_governor/
+    let rate_limiter_config = Box::new(
+        GovernorConfigBuilder::default()
+            .per_second(1)
+            .burst_size(2)
+            .methods(vec![http::Method::POST])
+            .finish()
+            .unwrap(),
+    );
+    let rate_limiter_layer = ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(|e: BoxError| async move {
+            display_error(e)
+        }))
+        .layer(GovernorLayer {
+            config: Box::leak(rate_limiter_config),
+        });
+
     let router = Router::new()
         .nest("/api", api_router().layer(TraceLayer::new_for_http()))
         .nest("/docs", Router::new().fallback(docs_handler))
         .fallback(frontend_handler)
-        .layer(CompressionLayer::new());
+        .layer(CompressionLayer::new())
+        .layer(rate_limiter_layer);
 
     // run it
     let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
     tracing::info!("listening on http://{addr}");
     axum::Server::bind(&addr)
-        .serve(router.into_make_service())
+        .serve(router.into_make_service_with_connect_info::<SocketAddr>())
         .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
